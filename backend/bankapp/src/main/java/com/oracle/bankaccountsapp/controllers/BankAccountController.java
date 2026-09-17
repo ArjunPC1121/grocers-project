8b74b719ede21ded1fc047c1c7db128c761c9407
Akash-Sakala <akashsakala04@gmail.com>
2026-09-17 15:48:18 +0530

    feat(bankapp): updated bankapp controller

--- backend/bankapp/src/main/java/com/oracle/bankaccountsapp/controllers/BankAccountController.java
+++ backend/bankapp/src/main/java/com/oracle/bankaccountsapp/controllers/BankAccountController.java
@@ -1,15 +1,16 @@
 package com.oracle.bankaccountsapp.controllers;
 
-import com.oracle.bankaccountsapp.dtos.AccountCreationRequest;
-import com.oracle.bankaccountsapp.dtos.DeductionRequest;
-import com.oracle.bankaccountsapp.dtos.DeductionResponse;
-import com.oracle.bankaccountsapp.services.abstractions.BankAccountService;
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.oracle.bankaccountsapp.dtos.AccountCreationRequest;
+import com.oracle.bankaccountsapp.services.abstractions.BankAccountService;
 
 @RestController
 @RequestMapping("/grocers/api/banks")
@@ -27,8 +28,8 @@ public class BankAccountController {
     }
 
     @PostMapping("/{userId}/deduct")
-    public ResponseEntity<DeductionResponse> deduct(@PathVariable int userId, @RequestBody DeductionRequest request) {
-        Double deductedAmount = bankAccountService.deduct(userId, request.amount());
-        return ResponseEntity.ok(new DeductionResponse(deductedAmount));
+    public ResponseEntity<Double> deduct(@PathVariable int userId, @RequestBody Map<String, Double> request) {
+        Double deductedAmount = bankAccountService.deduct(userId, request.get("amount"));
+        return ResponseEntity.ok(deductedAmount);
     }
 }
